ba757487bc900fd0d4403663b1d956650d84eaa1
Kim 金可明 <koomen@protonmail.com>
2023-06-28 09:26:35 +0200

    Update documentation

--- _static/js/enforce-light-theme.js
+++ _static/js/enforce-light-theme.js
@@ -1,21 +1,20 @@
-(($, window, document) => {
+function enforceLightMode() {
+  // Remove theme switcher, disallowing dark mode.
+  const switchButton = document.querySelector('.theme-switch-button');
+  if (typeof switchButton !== 'undefined') {
+    switchButton.remove();
+  }
 
-  $(window).load(function() {
-    // Remove theme switcher, disallowing dark mode.
-    const switchButton = $('.theme-switch-button');
-    if (switchButton.length > 0) {
-      switchButton.remove();
-    }
+  // Enforce light mode, because that is when our story looks the best :)
+  if (document.querySelector('html').getAttribute('data-theme') !== 'light') {
+    // Switch theme right now.
+    document.querySelector('html').setAttribute('data-theme', 'light')
 
-    // Enforce light mode, because that is when our story looks the best :)
-    if ($('html').attr('data-theme') !== 'light') {
-      // Switch theme right now.
-      $('html').attr('data-theme', 'light');
+    // Set light mode to be permanent for future visits.
+    localStorage.setItem('mode', 'light');
+    localStorage.setItem('theme', 'light');
 
-      // Set light mode to be permanent for future visits.
-      localStorage.setItem('mode', 'light');
-      localStorage.setItem('theme', 'light');
-    }
-  });
+  }
+}
 
-})(jQuery, window, document);
+window.addEventListener('load', enforceLightMode);
